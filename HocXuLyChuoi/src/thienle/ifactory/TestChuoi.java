150ecee75260b04a3695355db965e82be6ffd3e0
Thien Le <Thienlb15411@st.uel.edu.vn>
2019-12-22 21:31:25 +0700

    First check in Setting project

---
+++ HocXuLyChuoi/src/thienle/ifactory/TestChuoi.java
@@ -0,0 +1,11 @@
+package thienle.ifactory;
+
+public class TestChuoi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Chào mừng bạn đến với Github.com");
+	}
+
+}
